00e4d19aa5ff5bd61393793c6187f909fa06c666
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-02-02 20:42:33 -0600

    completed problem 5 with bonus

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -7,11 +7,27 @@ import java.io.PrintWriter;
 
 @WebServlet(name="CountServlet", urlPatterns = "/count")
 public class CountServlet extends HttpServlet {
+    static int counter = 0;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
+//        String reset = req.getParameter("reset");
+//        if(reset == "reset"){
+//            counter = 0;
+//        }else{
+//            String response = String.format("<h1>%d%n</h1>", counter++);
+//            out.println(response);
+//        }
 
-        
+        String reset = req.getParameter("reset");
+        if(reset == null){
+            String response = String.format("<h1>%d%n</h1>", counter++);
+            out.println(response);
+//            out.println("<h1> Hello, World!</h1>");
+        }else{
+            counter = 0;
+            out.printf("<h1>%d%n</h1>", counter);
+        }
     }
 }
